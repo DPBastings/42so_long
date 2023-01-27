786c7dd4251986148b422cefef099543376cc458
David Bastings <dbasting@f0r5s14.codam.nl>
2023-01-27 14:49:08 +0100

    Implemented a filename check (a map's name must end with the .ber extension).

--- source/map_read.c
+++ source/map_read.c
@@ -6,7 +6,7 @@
 /*   By: dbasting <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/16 16:20:41 by dbasting      #+#    #+#                 */
-/*   Updated: 2023/01/23 18:21:35 by dbasting      ########   odam.nl         */
+/*   Updated: 2023/01/27 14:36:18 by dbasting      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft.h"
-#include "libftprintf.h"
 
-static void	lst_destroy(t_list **list)
+static int		check_ext(char const *filename, char const *extension);
+static t_list	*get_list(int fd, size_t *width, size_t *height);
+static void		lst_destroy(t_list **list);
+static int		populate_map(t_map *map, t_list *list);
+
+static int	check_ext(char const *filename, char const *extension)
 {
-	t_list	*prev;
+	size_t	len;
+	size_t	ext_len;
 
-	while (*list)
-	{
-		prev = *list;
-		*list = (*list)->next;
-		free(prev);
-	}
-}
-
-static int	get_map(t_map *map, t_list *list)
-{
-	size_t	i;
-
-	map->objs = malloc((map->h + 1) * sizeof(char *));
-	if (map->objs == NULL)
-		return (map_destroy(&map), 0);
-	map->objs[map->h] = NULL;
-	i = 0;
-	while (list)
-	{
-		map->objs[i++] = list->content;
-		list = list->next;
-	}
-	return (1);
+	len = ft_strlen(filename);
+	ext_len = ft_strlen(extension);
+	if (len < ext_len + 1)
+		return (0);
+	return ((int) ft_strnstr(filename + len - ext_len, extension, ext_len));
 }
 
 static t_list	*get_list(int fd, size_t *width, size_t *height)
@@ -70,14 +57,48 @@ static t_list	*get_list(int fd, size_t *width, size_t *height)
 	return (list);
 }
 
+static void	lst_destroy(t_list **list)
+{
+	t_list	*prev;
+
+	while (*list)
+	{
+		prev = *list;
+		*list = (*list)->next;
+		free(prev);
+	}
+}
+
+static int	populate_map(t_map *map, t_list *list)
+{
+	size_t	i;
+
+	map->objs = malloc((map->h + 1) * sizeof(char *));
+	if (map->objs == NULL)
+		return (map_destroy(&map), 0);
+	map->objs[map->h] = NULL;
+	i = 0;
+	while (list)
+	{
+		map->objs[i++] = list->content;
+		list = list->next;
+	}
+	return (1);
+}
+
+#include "libftprintf.h"
+
 t_map	*map_read(char const *filename)
 {
 	t_map	*map;
 	t_list	*tmp;
 	int		fd;
 
-	if (!check_filename(filename, ".ber", ft_strlen(filename)))
+	if (!check_ext(filename, MAP_EXT))
+	{
+		ft_dprintf(2, "invalid filename\n");
 		return (NULL);
+	}
 	map = ft_calloc(1, sizeof(t_map));
 	if (map == NULL)
 		return (NULL);
@@ -88,7 +109,7 @@ t_map	*map_read(char const *filename)
 	close(fd);
 	if (tmp == NULL)
 		return (map_destroy(&map), NULL);
-	get_map(map, tmp);
+	populate_map(map, tmp);
 	lst_destroy(&tmp);
 	return (map);
 }
