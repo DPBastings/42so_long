4454b8e8b3960f57a342a7d7f961333b624095ba
D.P. Bastings <dp.bastings@posteo.net>
2023-03-23 21:40:39 +0100

    Removed 'W' from the array of valid map characters.

--- header/sl_map_check.h
+++ header/sl_map_check.h
@@ -16,17 +16,17 @@
 # include "sl_object.h"
 # include "sl_error.h"
 
-# define N_MAP_CHECKS	3
-
 # define SL_FILEEXT		".ber"
 
-# define CHR_ALL		"0PCE1HVW"
+# define CHR_ALL		"0PCE1HV"
 # define CHR_NONE		'0'
 # define N_CHR			8
 
 # define CHECK_EXIT		1
 # define CHECK_PLYR		2
 # define CHECK_COLL		4
+
+# define N_MAP_CHECKS	3
 
 t_sl_errno	map_check_walls(t_map *map);
 t_sl_errno	map_check_objects(t_map *map);
